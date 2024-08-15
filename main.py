21ffc27e500579d64942d0d1a66aa12b992c1643
Eduardo Schogor Ruiz <108836220+K4WTY@users.noreply.github.com>
2024-08-15 14:48:34 -0300

    Update main.py
    NEW ATT

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
     st.set_page_config(page_title="ESR - Auxílio", page_icon=":crown:")
     tab1, tab2, tab3, tab4 = st.tabs(["Receitas", "Cobranças", "Inadimplências", "Encargos de cobranças"])
     with st.sidebar:
-        st.subheader("Version - 13.08.2024 11:00")
+        st.subheader("Version - 15.08.2024 14:47")
         pdf_docs = st.file_uploader("Carregue seus arquivos em formato PDF", accept_multiple_files=True)
     with tab1:
         st.image("./imgs/exemplo1.png")
@@ -35,23 +35,41 @@ def main():
         naoAcrescenta = st.text_input("Mask Prova Real (Ex: 109/)", "")
         st.warning("Caso não puxe alguma informação alguma máscara pode estar errada!")
         if st.button("Puxar receitas"):
+
             totalAcres = 0
             totalAcresConc = ""
+            ng = False
             array = puxarTextoReceitas(pdf_docs, mascaraAcres, mascaraManual, naoAcrescenta)
+
             for i in range(len(array)):
+
                 if array[i] == "Receitas":
-                    sigla = ""
+
+                    sigla = array[i - 1]
+
                     if array[i - 1] == "(2NG)":
-                        sigla += array[i - 2] + " NG"
+                        sigla = array[i - 2]
                     elif array[i - 1] == ")":
-                        sigla += array[i - 2] + ")"
+                        sigla = array[i - 2] + ")"
                     elif array[i - 1] == "2)":
-                        sigla += array[i - 2] + " NG"
+                        sigla = array[i - 2] + ")"
+
+                    for j in range(20):
+                        if array[i - j] == "NG" or array[i - j] == "2NG":
+                            ng = True
+                            break
+                    
+                    st.write("-----------------------------------------")
+                    if ng:
+                        st.write(sigla + " (NG)")
                     else:
-                        sigla += array[i - 1]
+                        st.write(sigla)
+                    
+                    ng = False
+
                 if array[i] == "mascaraManual":
                     with tab1:
-                        original_title = '<p style="color:red; font-weight: bold;">' + sigla + ' Manual: ' + array[i + 2] + '</p>'
+                        original_title = '<p style="color:red; font-weight: bold;">' + ' Manual: ' + array[i + 2] + '</p>'
                         st.markdown(original_title, unsafe_allow_html=True)
                 if array[i] == "mascaraAcres":
                     numero = array[i + 3]
@@ -62,62 +80,81 @@ def main():
                         else:
                             totalAcresConc += "+" + numero
                         with tab1:
-                            st.write(sigla + " Acrés./Desc: " + numero)
+                            st.write("Acrés./Desc: " + numero)
                 if array[i] == "Total:":
                     with tab1:
                         if totalAcresConc != "":
                             st.code("=" + totalAcresConc, language="python")
                             totalAcresConc = ""
-                        st.write(sigla + " " + array[i] + " " + array[i + 1])
+                        st.write(array[i] + " " + array[i + 1])
                         if totalAcres != 0:
                             st.code("=" + array[i + 1] + "-" + str(totalAcres).replace('.',','), language="python")
-                        st.write("-----------------------------------------")
                         totalAcres = 0
     with tab2:
         if st.button("Puxar Cobranças"):
+
             array = puxarTexto(pdf_docs)
+
             for i in range(len(array)):
                 if array[i] == "Composição":
-                    sigla = ""
+                    
+                    sigla = array[i - 1]
+
                     if array[i - 1] == "(2NG)":
-                        sigla += array[i - 2] + " NG"
+                        sigla = array[i - 2]
                     elif array[i - 1] == ")":
-                        sigla += array[i - 2] + ")"
+                        sigla = array[i - 2] + ")"
                     elif array[i - 1] == "2)":
-                        sigla += array[i - 2] + " NG"
-                    else:
-                        sigla += array[i - 1]
+                        sigla = array[i - 2] + ")"
+
+                    st.write("-----------------------------------------")
+                    st.write(sigla)
+                
                 if array[i] == "Total":
                     with tab2:
-                        st.write(sigla + " A vencer: " + array[i + 1])
-                        st.write("-----------------------------------------")
+                        st.write("A vencer: " + array[i + 1])
+
     with tab3:
         if st.button("Puxar Inadimplências"):
+
             ng = False
             array = puxarTexto(pdf_docs)
+
             for i in range(len(array)):
-                if array[i] == "NG" or array[i] == "2NG":
-                    ng = True
+
                 if array[i] == "Inadimplência":
-                        if array[i - 1] == "2)" or array[i - 1] == ")":
-                            sigla = array[i - 2] + ")"
-                        elif array[i - 1] == "(2NG)":
-                            sigla = array[i - 2]
-                        else:
-                            sigla = array[i - 1]
+                        
+                    sigla = array[i - 1]
+
+                    if array[i - 1] == "(2NG)":
+                        sigla = array[i - 2]
+                    elif array[i - 1] == ")":
+                        sigla = array[i - 2] + ")"
+                    elif array[i - 1] == "2)":
+                        sigla = array[i - 2] + ")"
+
+                    for j in range(20):
+                        if array[i - j] == "NG" or array[i - j] == "2NG":
+                            ng = True
+                            break
+                    
+                    st.write("-----------------------------------------")
+                    if ng:
+                        st.write(sigla + " (NG)")
+                    else:
+                        st.write(sigla)
+                    
+                    ng = False
+
                 if array[i] == "inadimplentes" or array[i] == "inadimplente" or array[i] == "(0,00%)":
                     with tab3:
-                        if ng:
-                            st.write(sigla + " (NG)" + " Inadimplência: " + array[i + 2])
-                            st.write("-----------------------------------------")
-                            ng = False
-                        else:
-                            st.write(sigla + " Inadimplência: " + array[i + 2])
-                            st.write("-----------------------------------------")
+                        st.write("Inadimplência: " + array[i + 2])
+
     with tab4:
         if st.button("Puxar Encargos"):
+
             array = puxarTextoEncargos(pdf_docs)
-            siglaBackup = ""
+
             for i in range(len(array)):
                 if array[i] == "Composição":
                     sigla = ""
@@ -129,20 +166,23 @@ def main():
                         sigla += array[i - 2] + " NG"
                     else:
                         sigla += array[i - 1]
-                    if sigla != siglaBackup:
-                        st.write("-----------------------------------------")
-                        st.write(sigla)
-                        st.write("Encargos:")
-                        siglaBackup = sigla
+
+                    st.write("-----------------------------------------")
+                    st.write(sigla)
+                    st.write("Encargos:")
+
+
                 if array[i] == "encargosCob":
                     with tab4:
                         if array[i + 4] == "Total":
                             st.write("Total: " + array[i + 5])
                             break
                         st.code(array[i + 5], language="python")
+
                 if array[i] == "Total":
                     with tab4:
                         if array[i + 1] != "1.60.10":
                             st.write("Total: " + array[i + 1])
+
 if __name__ == "__main__":
     main()

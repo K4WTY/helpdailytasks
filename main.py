398789edd42800c4511fb50a38327e2dc041b4c8
Eduardo Schogor Ruiz <108836220+K4WTY@users.noreply.github.com>
2025-02-15 14:16:03 -0300

    NOVA ATT - 15/02/2025

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
         st.image("./imgs/exemplo2.png")
         mascaraManual = st.text_input("Mask Manual (Ex: 08/24 M)", "")
         st.image("./imgs/exemplo3.png")
-        naoAcrescenta = st.text_input("Mask Prova Real (Ex: 109/)", "")
+        naoAcrescenta = st.text_input("Mask Prova Real (Ex: 109/ (ITAU)) (Ex: 25/ (SICREDI))", "")
         st.warning("Caso não puxe alguma informação alguma máscara pode estar errada!")
         if st.button("Puxar receitas"):
 
@@ -93,8 +93,53 @@ def main():
                             totalAcresConc = ""
                         totalAcres = 0
     with tab2:
-        if st.button("Puxar Cobranças"):
+        if st.button("Puxar Cobranças G"):
 
+            siglasGarantidasCobrancas = [
+                '(ABA)',
+                '(ACO)',
+                '(CAP)',
+                '(CEN)',
+                '(GLS)',
+                '(GAS)',
+                '(GRE)',
+                '(GRY)',
+                '(GTU)',
+                '(GBA)',
+                '(GUA)',
+                '(HTO)',
+                '(LHA)',
+                '(IVO)',
+                '(JAN',
+                '(JDS)',
+                '(LEO)',
+                '(MEL)',
+                '(MAD)',
+                '(MCA)',
+                '(MAR)',
+                '(MTO)',
+                '(ODE)',
+                '(PAN)',
+                '(PDP)',
+                'PSV',
+                '(PRA)',
+                '(PDE)',
+                '(RAV)',
+                '(RAY)',
+                '(REI)',
+                '(SBA)',
+                '(EMI)',
+                '(SOL)',
+                '(TAR)',
+                '(TER)',
+                '(UBI)',
+                '(UNI)',
+                '(VIR)',
+                '(VFE)',
+            ]
+            
+            cobrancas = {}
+            st.write("------------------------------- G -------------------------------")
             array = puxarTexto(pdf_docs)
 
             for i in range(len(array)):
@@ -109,16 +154,139 @@ def main():
                     elif array[i - 1] == "2)":
                         sigla = array[i - 2] + ")"
 
-                    st.write("-----------------------------------------")
-                    st.write(sigla)
+                    cobrancas |= {sigla: 0}
+
+                if array[i] == "Total":
+                    cobrancas |= {sigla: array[i + 1]}
+
+            for i in range(len(siglasGarantidasCobrancas)):
+                if siglasGarantidasCobrancas[i] in cobrancas:
+                    st.code(cobrancas[siglasGarantidasCobrancas[i]])
+
+        if st.button("Puxar Cobranças NG"):
+                    
+            siglasNaoGarantidasCobrancas = [
+                '(ABA)',
+                '(ACO)',
+                '(CAP)',
+                '(CEN)',
+                '(GLS)',
+                '(GAS)',
+                '(GRE)',
+                '(GRY)',
+                '(GTU)',
+                '(IVO)',
+                '(JAN)',
+                '(MEL)',
+                '(MTO)',
+                '(ODE)',
+                '(PDP)',
+                '(PSV)',
+                '(PDE)',
+                '(RAY)',
+                '(SOL)',
+                '(UBI)',
+                '(UNI)',
+                '(VFE)'
+            ]
+
+            array = puxarTexto(pdf_docs)
+            cobrancas = {}
+            st.write("------------------------------- NG -------------------------------")
+
+            for i in range(len(array)):
+                if array[i] == "Composição":
+                    
+                    sigla = array[i - 1]
+
+                    if array[i - 1] == "(2NG)":
+                        sigla = array[i - 2]
+                    elif array[i - 1] == ")":
+                        sigla = array[i - 2] + ")"
+                    elif array[i - 1] == "2)":
+                        sigla = array[i - 2] + ")"
+
+                    cobrancas |= {sigla: 0}
                 
                 if array[i] == "Total":
-                    with tab2:
-                        st.write(array[i + 1])
+                    cobrancas |= {sigla: array[i + 1]}
+
+            for i in range(len(siglasNaoGarantidasCobrancas)):
+                if siglasNaoGarantidasCobrancas[i] in cobrancas:
+                    st.code(cobrancas[siglasNaoGarantidasCobrancas[i]])
 
     with tab3:
         if st.button("Puxar Inadimplências"):
+            
+            siglasGarantidasInads = [
+                '(ABA)',
+                '(ACO)',
+                '(CAP)',
+                '(CEN)',
+                '(GLS)',
+                '(GAS)',
+                '(GRE)',
+                '(GRY)',
+                '(GTU)',
+                '(GBA)',
+                '(GUA)',
+                '(HTO)',
+                '(LHA)',
+                '(IVO)',
+                '(JAN)',
+                '(JDS)',
+                '(LEO)',
+                '(MEL)',
+                '(MAD)',
+                '(MCA)',
+                '(MAR)',
+                '(MTO)',
+                '(ODE)',
+                '(PAN)',
+                '(PDP)',
+                '(PSV)',
+                '(PRA)',
+                '(PDE)',
+                '(RAV)',
+                '(RAY)',
+                '(REI)',
+                '(SBA)',
+                '(EMI)',
+                '(SOL)',
+                '(TAR)',
+                '(TER)',
+                '(UBI)',
+                '(UNI)',
+                '(VIR)',
+                '(VFE)',
+            ]
 
+            siglasNaoGarantidasInads = [
+                '(ABA) (NG)',
+                '(ACO) (NG)',
+                '(CAP) (NG)',
+                '(CEN) (NG)',
+                '(GLS) (NG)',
+                '(GAS) (NG)',
+                '(GRE) (NG)',
+                '(GRY) (NG)',
+                '(GTU) (NG)',
+                '(IVO) (NG)',
+                '(JAN) (NG)',
+                '(77) (NG)',
+                '(MTO) (NG)',
+                '(ODE) (NG)',
+                '(PDP) (NG)',
+                '(PSV) (NG)',
+                '(PDE) (NG)',
+                '(RAY) (NG)',
+                '(SOL) (NG)',
+                '(UBI) (NG)',
+                '(UNI) (NG)',
+                '(VFE) (NG)'
+            ]
+            
+            inads = {}
             ng = False
             array = puxarTexto(pdf_docs)
 
@@ -139,18 +307,30 @@ def main():
                         if array[i - j] == "NG" or array[i - j] == "2NG":
                             ng = True
                             break
-                    
-                    st.write("-----------------------------------------")
+
                     if ng:
-                        st.write(sigla + " (NG)")
-                    else:
-                        st.write(sigla)
+                        sigla = sigla + " (NG)"
                     
+                if array[i] == "inadimplentes" or array[i] == "inadimplente" or array[i] == "(0,00%)":
+                    if array[i + 2] == "W002B" or array[i + 2] == "CONDOMÍNIO":
+                        inads |= {sigla: 0}
+                    else:
+                        inads |= {sigla: array[i + 2]}
+
                     ng = False
 
-                if array[i] == "inadimplentes" or array[i] == "inadimplente" or array[i] == "(0,00%)":
-                    with tab3:
-                        st.write(array[i + 2])
+            st.write("------------------------------- G -------------------------------")
+
+            for i in range(len(siglasGarantidasInads)):
+                if siglasGarantidasInads[i] in inads:
+                    st.code(inads[siglasGarantidasInads[i]])
+            
+            st.write("------------------------------- NG -------------------------------")
+
+            for i in range(len(siglasNaoGarantidasInads)):
+                if siglasNaoGarantidasInads[i] in inads:
+                    st.code(inads[siglasNaoGarantidasInads[i]])
+
 
     with tab4:
         if st.button("Puxar Encargos"):
